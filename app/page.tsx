dd5f951a8b3c0d792096a54f77c3596ffd245100
HaranK007 <shreeharan2003@gmail.com>
2024-05-05 01:42:33 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react';
+import { AccessToken, Role } from "@huddle01/server-sdk/auth";
+import { createRoom } from "./roomCreate";
+import { useRoom, useLocalVideo, usePeerIds, useRemoteVideo } from '@huddle01/react/hooks';
+import { Video } from '@huddle01/react/components';
 
-export default function Home() {
+interface RemotePeerProps {
+  peerId: string;
+}
+
+const RemotePeer: React.FC<RemotePeerProps> = ({ peerId }) => {
+  const { stream: videoStream } = useRemoteVideo({ peerId });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      {videoStream && <Video stream={videoStream}></Video>}
+    </div>
   );
 }
+
+const Home: React.FC = () => {
+  const [roomId, setRoomId] = useState("");
+  const [token, setToken] = useState("");
+  const [inputValue, setInputValue] = useState('');
+
+  const { joinRoom, leaveRoom } = useRoom({
+    onJoin: () => { console.log('Joined the room'); },
+    onLeave: () => { console.log('Left the room'); },
+  });
+
+  const { enableVideo, disableVideo, isVideoOn } = useLocalVideo();
+  let { peerIds } = usePeerIds();
+
+  console.log(peerIds)
+  async function genAccessToken(roomId: string, roles: Role) {
+    const accessToken = new AccessToken({
+      apiKey: "O2f8knlUEX4P4OI26SDArlbxmKCdsPuK"!,
+      roomId: roomId,
+      role: roles,
+      permissions: {
+        admin: true,
+        canConsume: true,
+        canProduce: true,
+        canProduceSources: {
+          cam: true,
+          mic: true,
+          screen: true,
+        },
+        canRecvData: true,
+        canSendData: true,
+        canUpdateMetadata: true,
+      }
+    });
+
+    try {
+      const token = await accessToken.toJwt();
+      console.log(token);
+      setToken(token);
+    } catch (error) {
+      console.error("Error occurred while getting token:", error);
+    }
+  }
+
+  async function genRoomId() {
+    const roomId = await createRoom();
+    setRoomId(roomId as string);
+    console.log("Room ID generated:", roomId);
+  }
+
+  async function joinAsHost() {
+    await genAccessToken(inputValue, Role.HOST);
+    console.log("Joining as host...");
+    await joinRoom({
+      roomId: inputValue,
+      token: token
+    });
+    
+  }
+
+  async function joinAsCoHost() {
+    
+    await genAccessToken(inputValue, Role.CO_HOST);
+    console.log("Joining as co-host...");
+    await joinRoom({
+      roomId: inputValue,
+      token: token
+    });
+    
+  }
+
+  function leaveTheRoom() {
+    leaveRoom();
+    console.log("Leaving room...");
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.target.value);
+  }
+
+  return (
+
+  <div className="bg-gradient-to-b from-gray-200 to-gray-400 min-h-screen flex items-center justify-center">
+  <div className="flex justify-between w-full max-w-screen-lg">
+    <div className="flex flex-col items-start space-y-4">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={genRoomId}>Generate Room ID</button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => console.log("Room ID:", roomId)}>Show Room ID</button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        className="border rounded px-2 py-1"
+        placeholder="Enter Room ID"
+      />
+      <div className="flex space-x-4">
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={joinAsHost}>Join as Host</button>
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={joinAsCoHost}>Join as Co-host</button>
+      </div>
+      <button className={`bg-${isVideoOn ? 'red' : 'green'}-500 hover:bg-${isVideoOn ? 'red' : 'green'}-700 text-white font-bold py-2 px-4 rounded`} onClick={() => (isVideoOn ? disableVideo() : enableVideo())}>
+        {isVideoOn ? 'Disable Video' : 'Enable Video'}
+      </button>
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={leaveTheRoom}>Leave Room</button>
+    </div>
+    <div className="flex flex-wrap justify-center">
+      {peerIds != null && peerIds.length > 0 ? (
+        peerIds.map(peerId => (
+          <RemotePeer peerId={peerId}  />
+        ))
+      ) : (
+        <div className="w-48 h-48 flex items-center justify-center bg-gray-200">No videos turned on</div>
+      )}
+    </div>
+  </div>
+</div>
+
+  );
+}
+
+export default Home;
